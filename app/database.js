5650752b97760730607d2be29a1e00a1d1f82eeb
Carlos Mendoza <carlos.mendoza.91@gmail.com>
2020-08-25 12:58:32 -0700

    test

--- app/database.js
+++ app/database.js
@@ -6,8 +6,9 @@ dotenv.config()
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME
+    // host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    socketPath: process.env.DB_SOCKET_PATH
 };
 
 export const dbConnect = (params, callback) => {
